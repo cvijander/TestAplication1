b7016fcdb046942f8de09ab3d4ce78a8d87b0124
cvijander <cvija85@gmail.com>
2024-10-28 00:21:30 +0100

    Add project files.

---
+++ TestAplication1/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestAplication1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NewProducts()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestAplication1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestAplication1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
